cf76967572c1536fd55674f0fb4bbaef12c5c3e1
liu-yunfei <jasonliuyf@gmail.com>
2021-04-24 22:54:34 +0800

    add one script to tranform json

---
+++ DataCollectionPythonScript/getfeature.py
@@ -0,0 +1,75 @@
+import os
+import json
+flist=[flist for flist in os.listdir("../Data/converted/")]
+for item in flist:
+    f=open("../Data/converted/"+item,'r',encoding='UTF-8')
+    fdata=f.readlines()
+    for line in range(len(fdata)):
+        fdata[line]=fdata[line].strip()
+    string=' '.join(fdata)
+    #Find subject code
+    subjectCodePos=string[string.index("Subject Code"):].index("COMP")
+    subjectCode=(string[string.index("Subject Code"):])[subjectCodePos:subjectCodePos+8]
+    #Special case process
+    if (subjectCode.find(' ')!=-1):
+        subjectCode=(string[string.index("Subject Code"):])[subjectCodePos:subjectCodePos+4]+(string[string.index("Subject Code"):])[subjectCodePos+5:subjectCodePos+9]
+    #Find subject title
+    subjectTitle=string[string.index("Subject Title")+15:string.index("Credit")]
+    #Special case process
+    if (subjectTitle.find('Industrial Placement')!=-1):
+        subjectTitle='Industrial Placement'
+    #Find Pre-requisite
+    try:
+        preRequisite=string[string.index("Pre-requisite"):string.index("Exclusion")]
+    except ValueError:
+        preRequisite=string[string.index("Pre-requisite"):string.index("Pre-requisite")+150]
+    import re
+    #Find pre-requ subject code position
+    preSubjectPos=[substr.start() for substr in re.finditer('COMP', preRequisite)]
+    #preSubjectPos.extend(substr.start() for substr in re.finditer('AMA', preRequisite))
+    #Find pre-requ subject code number
+    preSubjectNumber=[preRequisite[item:item+9] for item in preSubjectPos]
+    #Special case process
+    for sub in range(len(preSubjectNumber)):
+        if preSubjectNumber[sub][4]==' ':
+            preSubjectNumber[sub]=preSubjectNumber[sub][0:4]+preSubjectNumber[sub][5:9]
+        else:
+            preSubjectNumber[sub]=preSubjectNumber[sub][0:8]
+    if subjectCode=='COMP4434':
+        preSubjectNumber=['AMA1104', 'COMP1011', 'COMP2011', 'COMP2411']
+    #Find credit value
+    try:
+        for char in string[string.index("Credit Value"):string.index("Credit Value")+20]:
+            if ord(char)>47 and ord(char)<58:
+                subjectCredit=char
+                break
+    #Special case for COMP4000
+    except ValueError:
+        subjectCredit="24 Training"
+    #Find subject code
+    subjectLevel=subjectCode[4]
+    #Process the content to plain text
+    wordList=re.split(' |\n',string)
+    for item in range(len(wordList)):
+        wordList[item]=''.join(list(filter(str.isalpha,wordList[item]))).lower()
+    wordList=list(filter(None,wordList))
+    wordListDict=dict()
+    #Tranform list to dictionary
+    for word in wordList:
+        try:
+            wordListDict[word]+=1
+        except KeyError:
+            wordListDict[word]=1
+    #Finalize the data
+    dataDict=dict()
+    dataDict['Subject Code']=subjectCode
+    dataDict['Subject Level']=subjectLevel
+    dataDict['Subject Credit']=subjectCredit
+    dataDict['Subject Title']=subjectTitle
+    dataDict['Subject Pre-requisite']=preSubjectNumber
+    dataDict['Subject Content']=wordListDict
+    #Transform data to json
+    dataJson=json.dumps(dataDict)
+    writeFile=open("../Data/pre-processed/"+subjectCode+".json",'w',encoding='UTF-8')
+    writeFile.write(dataJson)
+    writeFile.close()
