c18fca9acc9902057215838b5f8d6e6ee211ef06
Yunfei LIU <yunfei.liu@connect.polyu.hk>
2021-04-25 21:55:36 +0800

    try to create data structure

---
+++ Programs/course.h
@@ -0,0 +1,13 @@
+#ifndef COURSE_H
+#define COURSE_H
+#include<string>
+class Course {
+    public:
+        Course();
+        void SetID(int id);
+        void SetCode(std::string code);
+        void SetTitle();
+
+    
+};
+#endif
