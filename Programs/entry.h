70c99d6f02690d625645c282bafb81e980f9e771
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2021-04-28 19:34:29 +0800

    cmakelist update

---
+++ Programs/entry.h
@@ -0,0 +1,22 @@
+//
+// Created by 韩佳明 on 2021/4/28.
+//
+
+#ifndef PROGRAMS_ENTRY_H
+#define PROGRAMS_ENTRY_H
+
+class Entry {
+
+public:
+    Entry *next = nullptr;
+    Entry *prev = nullptr;
+
+private:
+    int number = 0;
+
+
+
+
+};
+
+#endif //PROGRAMS_ENTRY_H
